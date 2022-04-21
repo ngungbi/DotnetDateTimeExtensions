eb7bd5bbd85d4b0dd545f555131501ec203fda77
Harish <tidak_ada_alamat@yahoo.co.id>
2022-04-21 22:19:34 +0700

    add summary

--- DateTime/Extensions/DateTimeExtensions.cs
+++ DateTime/Extensions/DateTimeExtensions.cs
@@ -1,6 +1,11 @@
 namespace Ngb.DateTime.Extensions;
 
 public static class DateTimeExtensions {
+    /// <summary>
+    /// Convert DateTime to Unix epoch
+    /// </summary>
+    /// <param name="timestamp">Input timestamp</param>
+    /// <returns>Unix epoch seconds</returns>
     public static double ToUnixEpoch(this System.DateTime timestamp) {
         return (timestamp - System.DateTime.UnixEpoch).TotalSeconds;
     }
